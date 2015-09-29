b4a3ef483340426b36351a222ce87d9413f0d07c
Peter Klipfel <peter@klipfel.me>
2015-09-29 14:29:08 +0200

    installing docker, elasticsearch, kibana

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,32 @@
 #!/usr/bin/env bash
 
-apt-get update
-apt-get install -y nginx vim emacs postgresql-9.3 python-dev python-virtualenv
+function main {
+  apt-get update
+  apt-get install -y nginx vim emacs postgresql-9.3 python-dev python-virtualenv
 
+  install_docker
+  if docker ps | grep elasticsearch; then
+    echo "elasticsearch+kibna image already present, not redownloading"
+  else
+    docker pull nshou/elasticsearch-kibana
+  fi
+
+  if docker ps | grep elasticsearch; then
+    echo "elasticsearch+kibana is already running, not relaunching"
+  else
+    docker run -d -p 9200:9200 -p 5601:5601 nshou/elasticsearch-kibana
+  fi
+
+}
+
+function install_docker {
+  if docker --version | grep "Docker version"; then
+    echo "already installed docker - not reinstalling"
+  else
+    curl -sSL https://get.docker.com/ | sh
+  fi
+  echo "adding vagrant to the docker user group"
+  usermod -aG docker vagrant
+}
+
+main
